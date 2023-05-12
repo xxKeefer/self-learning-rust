1cb98453c6cf504f996921d48ba36ee6b62570b2
xxKeefer <xxkeefer.code@gmail.com>
2023-05-12 11:30:59 +1000

    feat: :sparkles: completed exercise 7
    working with enums

--- exercises/src/bin/a7.rs
+++ exercises/src/bin/a7.rs
@@ -10,4 +10,21 @@
 // * Use a match expression to determine which color
 //   name to print
 
-fn main() {}
+enum Color {
+    Red,
+    Green,
+    Blue,
+}
+
+fn display_color(color: Color) {
+    match color {
+        Color::Red => println!("red"),
+        Color::Green => println!("green"),
+        Color::Blue => println!("blue"),
+    }
+}
+
+fn main() {
+    let color = Color::Blue;
+    display_color(color)
+}
