1ed6018212b63f6587122ea8a4c85df021b1ff55
xxKeefer <xxkeefer.code@gmail.com>
2023-05-12 11:57:44 +1000

    feat: :sparkles: completed exercise 8
    working with structs

--- exercises/src/bin/a8.rs
+++ exercises/src/bin/a8.rs
@@ -9,4 +9,63 @@
 // * Use a function to print out the drink flavor and ounces
 // * Use a match expression to print the drink flavor
 
-fn main() {}
+enum TeaKind {
+    Lemon,
+    Herbal,
+    Black,
+    Green,
+}
+
+enum DrinkSize {
+    Sm,
+    Md,
+    Lg,
+}
+
+struct TeaDrink {
+    kind: TeaKind,
+    size: DrinkSize,
+}
+
+fn print_drink(drink: TeaDrink) {
+    let kind = match drink.kind {
+        TeaKind::Black => "Black Tea",
+        TeaKind::Lemon => "Lemon Tea",
+        TeaKind::Herbal => "Herbal Tea",
+        TeaKind::Green => "Green Tea",
+    };
+
+    let size = match drink.size {
+        DrinkSize::Lg => "large",
+        DrinkSize::Md => "medium",
+        DrinkSize::Sm => "small",
+    };
+
+    println!("You have selected a {} {}!", size, kind)
+}
+
+fn main() {
+    let black = TeaDrink {
+        size: DrinkSize::Lg,
+        kind: TeaKind::Black,
+    };
+    print_drink(black);
+
+    let herbal = TeaDrink {
+        size: DrinkSize::Md,
+        kind: TeaKind::Herbal,
+    };
+    print_drink(herbal);
+
+    let green = TeaDrink {
+        size: DrinkSize::Sm,
+        kind: TeaKind::Green,
+    };
+    print_drink(green);
+
+    let lemon = TeaDrink {
+        size: DrinkSize::Lg,
+        kind: TeaKind::Lemon,
+    };
+    print_drink(lemon);
+}
