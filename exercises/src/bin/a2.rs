1c98c8a46f2f7dda3dba5cdeaec9634a167ed462
xxKeefer <xxkeefer.code@gmail.com>
2023-05-12 10:27:10 +1000

    feat: :sparkles: completed exercise a2

--- exercises/src/bin/a2.rs
+++ exercises/src/bin/a2.rs
@@ -5,7 +5,16 @@
 //
 // Notes:
 // * Use a function to add two numbers together
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
 // * Use a function to display the result
 // * Use the "{:?}" token in the println macro to display the result
+fn display_result(result: i32) {
+    println!("The result is {:?}", result);
+}
 
-fn main() {}
+fn main() {
+    let result = add(5, 7);
+    display_result(result);
+}
