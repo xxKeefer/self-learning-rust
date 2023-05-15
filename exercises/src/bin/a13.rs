1ff0f72669ba5c9d0d2d5bac49044ab74ea80f7b
xxKeefer <xxkeefer.code@gmail.com>
2023-05-15 11:10:33 +1000

    feat: :sparkles: completed exercise 13
    working with vectors

--- exercises/src/bin/a13.rs
+++ exercises/src/bin/a13.rs
@@ -10,4 +10,14 @@
 // * Determine whether to print the number or print "thirty" inside the loop
 // * Use the .len() function to print the number of elements in a vector
 
-fn main() {}
+fn main() {
+    let numbers = vec![10, 20, 30, 40];
+
+    for n in &numbers {
+        match n {
+            30 => println!("thirty"),
+            _ => println!("{}", n),
+        }
+    }
+    println!("Total elements: {}", numbers.len())
+}
