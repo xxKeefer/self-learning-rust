a0f3cd6cc4190bdfdcde229e783fb8190c04d85f
xxKeefer <xxkeefer.code@gmail.com>
2023-05-15 12:16:58 +1000

    feat: :sparkles: completed exercise 14
    working with owned strings in structs with vectors

--- exercises/src/bin/a14.rs
+++ exercises/src/bin/a14.rs
@@ -11,4 +11,33 @@
 // * Use an if expression to determine which person's info should be printed
 // * The name and colors should be printed using a function
 
-fn main() {}
+struct Person {
+    age: i32,
+    name: String,
+    color: String,
+}
+
+impl Person {
+    fn new(age: i32, name: String, color: String) -> Self {
+        Self { age, name, color }
+    }
+
+    fn print_info(&self) {
+        println!("{}: {}", &self.name, &self.color)
+    }
+}
+
+fn main() {
+    let people = vec![
+        Person::new(9, String::from("Dan"), String::from("red")),
+        Person::new(10, String::from("Ben"), String::from("blue")),
+        Person::new(11, String::from("Sam"), String::from("green")),
+        Person::new(12, String::from("Ian"), String::from("yellow")),
+    ];
+
+    for p in people {
+        if p.age <= 10 {
+            p.print_info();
+        }
+    }
+}
