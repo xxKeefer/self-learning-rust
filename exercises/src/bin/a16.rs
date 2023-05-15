38b8a82f8bb97f9b88a5afc72ba31e37259daec5
xxKeefer <xxkeefer.code@gmail.com>
2023-05-15 15:37:03 +1000

    feat: :sparkles: complete exercise 16
    working with options

--- exercises/src/bin/a16.rs
+++ exercises/src/bin/a16.rs
@@ -11,4 +11,33 @@
 // * Use a struct containing the student's name and locker assignment
 // * The locker assignment should use an Option<i32>
 
-fn main() {}
+struct Student {
+    name: String,
+    locker_no: Option<i32>,
+}
+
+impl Student {
+    fn print_locker(&self) {
+        match &self.locker_no {
+            Some(locker) => println!("{}'s locker number is: {}", self.name, locker),
+            None => println!("{} has no locker number assigned,", self.name),
+        }
+    }
+}
+
+fn main() {
+    let sam = Student {
+        name: String::from("Sam"),
+        locker_no: Some(32),
+    };
+    let dan = Student {
+        name: String::from("Dan"),
+        locker_no: None,
+    };
+
+    let students = vec![sam, dan];
+
+    for s in students {
+        s.print_locker()
+    }
+}
